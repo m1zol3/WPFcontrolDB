b90f9a9e0d995269088d1b3fbefba64f00f106de
m1zol3 <michal_zoltowski@web.de>
2018-01-27 00:35:14 +0100

    Sync

--- WPFcontrolDB/pageWPF.cpp
+++ WPFcontrolDB/pageWPF.cpp
@@ -1,5 +1,8 @@
 #include "stdafx.h"
 #include "pageWPF.h"
+#include <windows.h>
+#include <CommCtrl.h>
+
  //Version 5
   
 pageWPF::pageWPF(){}
@@ -7,8 +10,7 @@ pageWPF::pageWPF(){}
 pageWPF::pageWPF(int setWidth, int setHeight ) {
     array<ColumnDefinition ^> ^ columnDef = gcnew array<ColumnDefinition ^>(8);
     array<RowDefinition ^> ^ rowDef = gcnew array<RowDefinition ^>(200);
-  
-   
+ 
     this->Height = setHeight;
     this->Width = setWidth;
     this->Background = gcnew SolidColorBrush(Colors::LightGray);
@@ -16,7 +18,6 @@ pageWPF::pageWPF(int setWidth, int setHeight ) {
     {
         columnDef[i] = gcnew ColumnDefinition();
         columnDef[i]->Width = GridLength(1, GridUnitType::Auto);
-        
         this->ColumnDefinitions->Add(columnDef[i]);
     }
     for (int i = 0; i<200; i++)
@@ -24,25 +25,51 @@ pageWPF::pageWPF(int setWidth, int setHeight ) {
         rowDef[i] = gcnew RowDefinition();
         rowDef[i]->Height = GridLength(1, GridUnitType::Auto);
         this->RowDefinitions->Add(rowDef[i]);
-        ID = CreateLabelID(1, i+1, (i+1).ToString());
-        this->Children->Add(ID); 
     }
-    ID = CreateLabel(1, 0, "ID");
+   /* myButton = CreateButton(0, 0, "Hallo");
+    this->Children->Add(myButton);
+   */ 
+    ID = CreateLabel(0, 1, "ID");
     this->Children->Add(ID);
-    firstname = CreateLabel(2, 0, "Vorname");
+    firstname = CreateLabel(1, 1, "Vorname");
     this->Children->Add(firstname);
-    lastname = CreateLabel(3,0, "Name");
+    lastname = CreateLabel(2,1, "Name");
     this->Children->Add(lastname);
-    status = CreateLabel(4, 0, "Status");
+    status = CreateLabel(3, 1, "Status");
     this->Children->Add(status);
-    teamPool = CreateLabel(5, 0, "Pool Mannschaft");
+    teamPool = CreateLabel(4, 1, "Pool Mannschaft");
     this->Children->Add(teamPool);
-    teamSnooker = CreateLabel(6, 0, "Snooker Mannschaft");
+    teamSnooker = CreateLabel(5, 1, "Snooker Mannschaft");
     this->Children->Add(teamSnooker);
-    date = CreateLabel(7, 0, "Eintrittsdatum");
+    date = CreateLabel(6, 1, "Eintrittsdatum");
     this->Children->Add(date);
- 
+  
+    myCombo = CreateCombo(1,0, "klh");
+    this->Children->Add(myCombo);
+    
+    CreateItem("Alle");
+    CreateItem("Aktiv");
+    CreateItem("Passiv");
+    CreateItem("Gast");
+    CreateItem("Ehemalige");
 
+
+    for (int i = 1 ;i<200;i++){
+    ID = CreateLabelID(0, i+1, (i).ToString());
+    this->Children->Add(ID);
+    }
+
+   
+
+
+}
+Button ^pageWPF::CreateButton(int column, int row, String ^text) {
+    Button ^newButton = gcnew Button();
+    newButton->Content = text;
+    newButton->Name = text;
+    Grid::SetColumn(newButton, column);
+    Grid::SetRow(newButton, row);
+    return newButton;
 }
 Label ^pageWPF::CreateLabel(int column, int row, String ^ text)
 {
@@ -68,4 +95,23 @@ Label ^pageWPF::CreateLabelID(int column, int row, String ^ text)
     Grid::SetRow(newLabel, row);
     return newLabel;
 }
- 
+ComboBox ^pageWPF::CreateCombo(int column, int row, String ^ text) {
+    ComboBox ^newCombo = gcnew ComboBox();
+    //newCombo->add
+   
+    
+    Grid::SetColumn(newCombo, column);
+    Grid::SetRow(newCombo, row);
+    return newCombo;
+
+
+ }
+
+ComboBoxItem ^ pageWPF::CreateItem(String ^ text)
+{
+    ComboBoxItem ^newItem = gcnew ComboBoxItem();
+
+    myCombo->Items->Add(text);
+    return newItem;
+    
+}
--- WPFcontrolDB/pageWPF.h
+++ WPFcontrolDB/pageWPF.h
@@ -7,7 +7,7 @@ using namespace System::Windows::Documents;
 using namespace System::Threading;
 using namespace System::Windows::Controls;
 using namespace System::Windows::Media;
-
+ 
 public ref class  DBPageEventArgs : EventArgs
 {
 public:
@@ -24,10 +24,18 @@ private:
     List myList;
 public:
  
+   
     pageWPF();
     pageWPF(int height, int width);
+    Button ^ myButton;
+    ComboBox ^ myCombo;
+    ComboBoxItem ^aktiv, ^passiv, ^gast, ehemaliger;
     Label ^ CreateLabel(int column, int row, String ^text);
     Label ^ CreateLabelID(int column, int row, String ^text);
+    Button ^CreateButton(int column, int row, String ^text);
+    ComboBox ^CreateCombo(int column, int row, String ^text);
+    ComboBoxItem ^CreateItem(String ^text);
+    
     
 };
 
