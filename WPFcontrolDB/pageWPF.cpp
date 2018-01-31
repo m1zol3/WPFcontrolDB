f9653a76c9fe5d3d1c8288e8092e068aadc6054c
m1zol3 <michal_zoltowski@web.de>
2018-01-31 08:32:50 +0100

    Add ScrollViewer, columns & rows

--- WPFcontrolDB/pageWPF.cpp
+++ WPFcontrolDB/pageWPF.cpp
@@ -2,67 +2,115 @@
 #include "pageWPF.h"
 #include <windows.h>
 #include <CommCtrl.h>
-
+#include <string.h>
  //Version 5
   
 pageWPF::pageWPF(){}
 
 pageWPF::pageWPF(int setWidth, int setHeight ) {
-    array<ColumnDefinition ^> ^ columnDef = gcnew array<ColumnDefinition ^>(8);
-    array<RowDefinition ^> ^ rowDef = gcnew array<RowDefinition ^>(200);
- 
+
+     array<ColumnDefinition ^> ^ columnDef = gcnew array<ColumnDefinition ^>(8);
+     array<RowDefinition ^> ^ rowDef = gcnew array<RowDefinition ^>(200);
+     ColumnDefinition ^mycolumnDefOne = gcnew ColumnDefinition();
+     ColumnDefinition ^mycolumnDefTwo = gcnew ColumnDefinition();
+     ColumnDefinition ^mycolumnDefThree = gcnew ColumnDefinition();
+     ScrollViewer ^myWindow = gcnew ScrollViewer();
+     StackPanel ^myStackPanel  = gcnew StackPanel();
+     Grid ^myGrid = gcnew Grid();
+
+     for (int i = 0; i<8; i++)
+     {
+         columnDef[i] = gcnew ColumnDefinition();
+         columnDef[i]->Width = GridLength(1, GridUnitType::Auto);
+         myGrid->ColumnDefinitions->Add(columnDef[i]);
+     }
+     for (int i = 0; i<200; i++)
+     {
+         rowDef[i] = gcnew RowDefinition();
+         rowDef[i]->Height = GridLength(1, GridUnitType::Auto);
+         myGrid->RowDefinitions->Add(rowDef[i]);
+     }
+      
+     myGrid->Width = 600;
+     myGrid->HorizontalAlignment = System::Windows::HorizontalAlignment::Left;
+     myGrid->VerticalAlignment = System::Windows::VerticalAlignment::Top;
+     myGrid->ShowGridLines = true;
+     
+     
+
+
+      
+
+     myWindow->Height = setHeight;
+     myWindow->Width = setWidth;
+
+    
     this->Height = setHeight;
     this->Width = setWidth;
+     
     this->Background = gcnew SolidColorBrush(Colors::LightGray);
-    for (int i = 0; i<8; i++)
-    {
-        columnDef[i] = gcnew ColumnDefinition();
-        columnDef[i]->Width = GridLength(1, GridUnitType::Auto);
-        this->ColumnDefinitions->Add(columnDef[i]);
-    }
-    for (int i = 0; i<200; i++)
-    {
-        rowDef[i] = gcnew RowDefinition();
-        rowDef[i]->Height = GridLength(1, GridUnitType::Auto);
-        this->RowDefinitions->Add(rowDef[i]);
-    }
-   /* myButton = CreateButton(0, 0, "Hallo");
-    this->Children->Add(myButton);
-   */ 
-    ID = CreateLabel(0, 1, "ID");
-    this->Children->Add(ID);
-    firstname = CreateLabel(1, 1, "Vorname");
-    this->Children->Add(firstname);
-    lastname = CreateLabel(2,1, "Name");
-    this->Children->Add(lastname);
-    status = CreateLabel(3, 1, "Status");
-    this->Children->Add(status);
-    teamPool = CreateLabel(4, 1, "Pool Mannschaft");
-    this->Children->Add(teamPool);
-    teamSnooker = CreateLabel(5, 1, "Snooker Mannschaft");
-    this->Children->Add(teamSnooker);
-    date = CreateLabel(6, 1, "Eintrittsdatum");
-    this->Children->Add(date);
-  
-    myCombo = CreateCombo(1,0, "klh");
-    this->Children->Add(myCombo);
-    
+ 
+    //this->Children->Add(status);
+    teamPool = CreateLabel(  "Pool Mannschaft");
+    //this->Children->Add(teamPool);
+    teamSnooker = CreateLabel( "Snooker Mannschaft");
+    //this->Children->Add(teamSnooker);
+    date = CreateLabel(  "Eintrittsdatum");
+    //this->Children->Add(date);
+   
+     
+    //this->Children->Add(myCombo);
+    myCombo = CreateCombo(1, 0, "klh");
     CreateItem("Alle");
     CreateItem("Aktiv");
     CreateItem("Passiv");
     CreateItem("Gast");
     CreateItem("Ehemalige");
+ 
 
 
-    for (int i = 1 ;i<200;i++){
-    ID = CreateLabelID(0, i+1, (i).ToString());
-    this->Children->Add(ID);
-    }
 
+    status = CreateLabel("ID");
+    firstname = CreateLabel("Vorname");
+    lastname = CreateLabel("Name");
+ 
+    myGrid->SetColumn(status, 0);
+    myGrid->SetRow(status, 1);
+    myGrid->SetColumn(firstname, 1);
+    myGrid->SetRow(firstname, 1);
+    myGrid->SetColumn(lastname, 2);
+    myGrid->SetRow(lastname, 1);
+    myGrid->Children->Add(status);
+    myGrid->Children->Add(firstname);
+    myGrid->Children->Add(lastname);
+
+     
+    
+    
+    myStackPanel->Children->Add(myGrid);
+    
+    this->Children->Add(myWindow);// ->Parent = pageWPF;
+    
+    
+    
    
+   // myStackPanel->Children->Add(firstname);
+  
+      for (int i = 1 ;i<100;i++){
+        ID = CreateLabelID(0,0, (i).ToString());
+        myGrid->SetRow(ID, i+2);
+        myGrid->SetColumn(ID, 0);
+   
+        myGrid->Children->Add(ID);
 
-
-}
+    } 
+  
+    myStackPanel->Width = 1000;
+    myStackPanel->HorizontalAlignment = System::Windows::HorizontalAlignment::Left;
+    myWindow->Content = myStackPanel;
+ 
+   
+} 
 Button ^pageWPF::CreateButton(int column, int row, String ^text) {
     Button ^newButton = gcnew Button();
     newButton->Content = text;
@@ -71,7 +119,7 @@ Button ^pageWPF::CreateButton(int column, int row, String ^text) {
     Grid::SetRow(newButton, row);
     return newButton;
 }
-Label ^pageWPF::CreateLabel(int column, int row, String ^ text)
+Label ^pageWPF::CreateLabel(  String ^ text)
 {
     Label ^ newLabel = gcnew Label();
     newLabel->Content = text;
@@ -79,8 +127,7 @@ Label ^pageWPF::CreateLabel(int column, int row, String ^ text)
     newLabel->FontWeight = FontWeights::ExtraBold;
     newLabel->FontSize = 18;
     newLabel->Background = gcnew SolidColorBrush(Colors::Gray);
-    Grid::SetColumn(newLabel, column);
-    Grid::SetRow(newLabel, row);
+    
     return newLabel;
 }
 Label ^pageWPF::CreateLabelID(int column, int row, String ^ text)
@@ -93,6 +140,7 @@ Label ^pageWPF::CreateLabelID(int column, int row, String ^ text)
     newLabel->Background = gcnew SolidColorBrush(Colors::Gray);
     Grid::SetColumn(newLabel, column);
     Grid::SetRow(newLabel, row);
+    
     return newLabel;
 }
 ComboBox ^pageWPF::CreateCombo(int column, int row, String ^ text) {
@@ -115,3 +163,22 @@ ComboBoxItem ^ pageWPF::CreateItem(String ^ text)
     return newItem;
     
 }
+
+ListView ^ pageWPF::CreateListView(int setHeigh, int setWidth)
+{
+    ListView ^myList = gcnew ListView;
+    myList->Height = setHeigh;
+    myList->Width = setWidth;
+    return myList;
+    // TODO: hier Rückgabeanweisung eingeben
+}
+
+ScrollViewer ^ pageWPF::CreatScroll(int setWidth, int setHeigh)
+{
+    ScrollViewer ^myScroll = gcnew ScrollViewer();
+    myScroll->Width = setWidth -30 ;
+    myScroll-> Height = setHeigh -30;
+    return myScroll;
+    // TODO: hier Rückgabeanweisung eingeben
+}
+ 
--- WPFcontrolDB/pageWPF.h
+++ WPFcontrolDB/pageWPF.h
@@ -3,6 +3,7 @@
  
 using namespace System;
 using namespace System::Windows;
+using namespace System::Windows::Navigation;
 using namespace System::Windows::Documents;
 using namespace System::Threading;
 using namespace System::Windows::Controls;
@@ -27,15 +28,21 @@ public:
    
     pageWPF();
     pageWPF(int height, int width);
+    ScrollViewer ^ myScroll;
     Button ^ myButton;
     ComboBox ^ myCombo;
     ComboBoxItem ^aktiv, ^passiv, ^gast, ehemaliger;
-    Label ^ CreateLabel(int column, int row, String ^text);
+    Label ^ CreateLabel( String ^text);
     Label ^ CreateLabelID(int column, int row, String ^text);
     Button ^CreateButton(int column, int row, String ^text);
     ComboBox ^CreateCombo(int column, int row, String ^text);
     ComboBoxItem ^CreateItem(String ^text);
+    ListView ^myListView;
+    ListView ^CreateListView(int setHeigh, int setWidth);
+    ScrollViewer ^CreatScroll(int setWidth, int setHeigh);
     
-    
+  
+
+                     
 };
 
